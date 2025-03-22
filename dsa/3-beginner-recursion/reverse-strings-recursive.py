dbdee84de81d311d594892fbc2f1f943773cd8f2
jimmy-do <jimmydo07@yahoo.com>
2025-03-21 23:34:13 -0500

    linked listies I

--- dsa/3-beginner-recursion/reverse-strings-recursive.py
+++ dsa/3-beginner-recursion/reverse-strings-recursive.py
@@ -6,7 +6,7 @@
 def reverse_string(s):
     if len(s) == 0:
         return ""
-    return s[-1]+reverse_string(s[:-1])
+    return s[-1] + reverse_string(s[:-1])
     # return reverse_string(s[1:])+s[0]
 
 
