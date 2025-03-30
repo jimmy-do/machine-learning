4468a3680950deb747f3173c7703f308eebd0a8c
jimmy-do <jimmydo07@yahoo.com>
2025-03-29 19:51:10 -0500

    alo moto

---
+++ dsa/4-binary-tree-I/tree-min-value.py
@@ -0,0 +1,9 @@
+# tree min value
+
+# Write a function, tree_min_value, that takes in the root of a binary tree that contains 
+# number values. The function should return the minimum value within the tree.
+
+# You may assume that the input tree is non-empty.
+
+def tree_min_value(root):
+    pass
